705c8b68e49e9611fd9bb91caad1e9bf9e0d7492
WithersChat <76114055+WithersChat@users.noreply.github.com>
2025-03-14 14:20:26 +0100

    KJS integration 90% done (the mod probably doesn't register the integration at all in its current state and it adds no actual methods yet)

---
+++ src/main/java/net/neganote/monilabs/integration/kjs/MoniKubeJSPlugin.java
@@ -0,0 +1,43 @@
+package net.neganote.monilabs.integration.kjs;
+
+import com.gregtechceu.gtceu.api.registry.GTRegistries;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.util.ClassFilter;
+import net.neganote.monilabs.integration.kjs.recipe.MoniRecipeSchema;
+
+public class MoniKubeJSPlugin extends KubeJSPlugin {
+
+    @Override
+    public void initStartup() {
+        super.initStartup();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void registerClasses(ScriptType type, ClassFilter filter) {
+        super.registerClasses(type, filter);
+        // allow user to access all gtceu classes by importing them.
+        filter.allow("net.neganote.monilabs");
+    }
+
+    @Override
+    public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
+        for (var entry : GTRegistries.RECIPE_TYPES.entries()) {
+            event.register(entry.getKey(), MoniRecipeSchema.SCHEMA);
+        }
+    }
+
+    /*@Override
+    public void registerBindings(BindingsEvent event) {
+        super.registerBindings(event);
+        event.add("MysteriousString", MysteriousClass.class);
+    }*/
+}
+
