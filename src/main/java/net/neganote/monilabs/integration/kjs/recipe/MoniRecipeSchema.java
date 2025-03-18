0cce551d2ece2ff5ba7c0f57d131edec7141021c
NegaNote <neganote43@gmail.com>
2025-03-18 19:10:20 -0400

    change specialcase schema entries to fix potential bug

--- src/main/java/net/neganote/monilabs/integration/kjs/recipe/MoniRecipeSchema.java
+++ src/main/java/net/neganote/monilabs/integration/kjs/recipe/MoniRecipeSchema.java
@@ -49,7 +49,7 @@ public interface MoniRecipeSchema {
         }
 
         // Used to have a shorthand for special cases in recipe definitions
-        public GTRecipeSchema.GTRecipeJS inputStates(SpecialCase specialCase) {
+        public GTRecipeSchema.GTRecipeJS inputSpecialCase(SpecialCase specialCase) {
             return switch (specialCase) {
                 case PRIMARY -> this.inputStates(0, 4, 8); // Red, Green, Blue
                 case SECONDARY -> this.inputStates(2, 6, 10); // Yellow, Cyan, Magenta
@@ -89,7 +89,7 @@ public interface MoniRecipeSchema {
 
 
         // Used to have a shorthand for special cases in recipe definitions
-        public GTRecipeSchema.GTRecipeJS outputStates(SpecialCase specialCase) {
+        public GTRecipeSchema.GTRecipeJS outputSpecialCase(SpecialCase specialCase) {
             return switch (specialCase) {
                 case PRIMARY -> this.outputStates(0, 4, 8); // Red, Green, Blue
                 case SECONDARY -> this.outputStates(2, 6, 10); // Yellow, Cyan, Magenta
