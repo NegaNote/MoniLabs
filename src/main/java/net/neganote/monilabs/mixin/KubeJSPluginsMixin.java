889e29857dcc44e7d357f9aac71d0fb60cbd1512
Athena Parker <neganote43@gmail.com>
2025-03-18 15:09:03 -0400

    Change mixin, doesn't seem to load?

--- src/main/java/net/neganote/monilabs/mixin/KubeJSPluginsMixin.java
+++ src/main/java/net/neganote/monilabs/mixin/KubeJSPluginsMixin.java
@@ -1,28 +1,24 @@
 package net.neganote.monilabs.mixin;
 
+import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.util.KubeJSPlugins;
 import dev.architectury.platform.Mod;
 import dev.architectury.platform.Platform;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mixin(KubeJSPlugins.class)
 public class KubeJSPluginsMixin {
-    @Shadow
-    private static void load(Mod mod, boolean loadClientPlugins) {}
-
     @Inject(method= "load(Ljava/util/List;Z)V", at = @At(value = "HEAD"))
     private static void beforeLoad(List<Mod> mods, boolean loadClientPlugins, CallbackInfo ci) {
+        KubeJS.LOGGER.debug("Attempting MoniLabs mixin...");
         mods.remove(Platform.getMod("monilabs"));
-    }
-
-    @Inject(method= "load(Ljava/util/List;Z)V", at = @At(value = "RETURN"))
-    private static void afterLoad(List<Mod> mods, boolean loadClientPlugins, CallbackInfo ci) {
-        load(Platform.getMod("monilabs"), loadClientPlugins);
+        mods.add(Platform.getMod("monilabs"));
+        KubeJS.LOGGER.debug(mods.stream().map(Mod::getModId).collect(Collectors.joining("\n")));
     }
 }
