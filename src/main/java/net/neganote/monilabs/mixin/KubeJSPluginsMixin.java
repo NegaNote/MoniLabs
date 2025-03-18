7b98aa96461ce7e6f26ce90bf42afad423a6e4ef
NegaNote <neganote43@gmail.com>
2025-03-18 15:30:11 -0400

    Change mixin, doesn't seem to load?

--- src/main/java/net/neganote/monilabs/mixin/KubeJSPluginsMixin.java
+++ src/main/java/net/neganote/monilabs/mixin/KubeJSPluginsMixin.java
@@ -1,10 +1,12 @@
 package net.neganote.monilabs.mixin;
 
-import dev.latvian.mods.kubejs.KubeJS;
 import dev.latvian.mods.kubejs.util.KubeJSPlugins;
 import dev.architectury.platform.Mod;
 import dev.architectury.platform.Platform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,11 +16,15 @@ import java.util.stream.Collectors;
 
 @Mixin(KubeJSPlugins.class)
 public class KubeJSPluginsMixin {
+    @Unique
+    private static Logger moniLabs$LOGGER = LoggerFactory.getLogger("KubeJSMoniMixin");
+
     @Inject(method= "load(Ljava/util/List;Z)V", at = @At(value = "HEAD"))
     private static void beforeLoad(List<Mod> mods, boolean loadClientPlugins, CallbackInfo ci) {
-        KubeJS.LOGGER.debug("Attempting MoniLabs mixin...");
-        mods.remove(Platform.getMod("monilabs"));
-        mods.add(Platform.getMod("monilabs"));
-        KubeJS.LOGGER.debug(mods.stream().map(Mod::getModId).collect(Collectors.joining("\n")));
+        moniLabs$LOGGER.debug("Attempting MoniLabs mixin...");
+        int index = mods.indexOf(Platform.getMod("monilabs"));
+        Mod mod = mods.remove(index);
+        mods.add(mod);
+        moniLabs$LOGGER.debug(mods.stream().map(Mod::getModId).collect(Collectors.joining("\n")));
     }
 }
