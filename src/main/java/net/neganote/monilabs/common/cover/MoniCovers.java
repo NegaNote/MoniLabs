c10c31e1fd3236399479d3ca6d9d4ff4d06818e2
NegaNote <neganote43@gmail.com>
2025-06-04 15:25:24 -0400

    add textures + renderer for chroma detector cover

--- src/main/java/net/neganote/monilabs/common/cover/MoniCovers.java
+++ src/main/java/net/neganote/monilabs/common/cover/MoniCovers.java
@@ -1,14 +1,13 @@
 package net.neganote.monilabs.common.cover;
 
-import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.cover.CoverDefinition;
 import com.gregtechceu.gtceu.api.item.ComponentItem;
 import com.gregtechceu.gtceu.api.registry.GTRegistries;
 import com.gregtechceu.gtceu.client.renderer.cover.ICoverRenderer;
-import com.gregtechceu.gtceu.client.renderer.cover.SimpleCoverRenderer;
 import com.gregtechceu.gtceu.common.item.CoverPlaceBehavior;
 
 import net.neganote.monilabs.MoniLabs;
+import net.neganote.monilabs.client.renderer.cover.ChromaDetectorCoverRenderer;
 import net.neganote.monilabs.common.cover.detector.ChromaDetectorCover;
 import net.neganote.monilabs.common.item.MoniItems;
 
@@ -20,7 +19,7 @@ import static net.neganote.monilabs.MoniLabs.REGISTRATE;
 public class MoniCovers {
 
     public final static CoverDefinition CHROMA_DETECTOR_COVER = register("chroma_detector", ChromaDetectorCover::new,
-            new SimpleCoverRenderer(GTCEu.id("block/cover/overlay_energy_detector")));
+            new ChromaDetectorCoverRenderer("block/cover/overlay_chroma_detector_"));
     // TODO: replace with our own overlay
 
     public static ItemEntry<ComponentItem> CHROMA_DETECTOR_ITEM = REGISTRATE
