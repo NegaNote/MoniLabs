6b5da6e89b82ddc74e68be5bf38274967411f493
NegaNote <neganote43@gmail.com>
2025-06-26 19:26:42 -0400

    tier 1 and 2 done

--- src/main/java/net/neganote/monilabs/client/renderer/MicroverseProjectorRenderer.java
+++ src/main/java/net/neganote/monilabs/client/renderer/MicroverseProjectorRenderer.java
@@ -24,7 +24,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import java.util.List;
 import java.util.function.Consumer;
 
-@SuppressWarnings("unused")
+// @SuppressWarnings("unused")
 public class MicroverseProjectorRenderer extends WorkableCasingMachineRenderer {
 
     public MicroverseProjectorRenderer(ResourceLocation baseCasing, ResourceLocation workableModel) {
@@ -45,20 +45,85 @@ public class MicroverseProjectorRenderer extends WorkableCasingMachineRenderer {
 
             Direction upwards = RelativeDirection.UP.getRelative(frontFacing, upwardsFacing, projector.isFlipped());
 
-            renderMicroverse(stack, buffer, upwards, frontFacing, combinedLight, combinedOverlay);
+            Direction left = RelativeDirection.LEFT.getRelative(frontFacing, upwardsFacing, projector.isFlipped());
+
+            int tier = projector.getTier();
+
+            renderMicroverse(stack, buffer, upwards, frontFacing, left, combinedLight, combinedOverlay, tier);
         }
     }
 
-    private void renderMicroverse(PoseStack stack, MultiBufferSource bufferSource, Direction upwards, Direction front,
-                                  int combinedLight, int combinedOverlay) {
+    private void renderMicroverse(PoseStack stack, MultiBufferSource buffer, Direction upwards, Direction front,
+                                  Direction left, int combinedLight, int combinedOverlay, int tier) {
+        switch (tier) {
+            case 1:
+                renderCuboid(stack, buffer, upwards, front, left, combinedLight, combinedOverlay, -1, 1, 0,
+                        1.008f, 1.008f, 1.008f);
+                break;
+            case 2:
+                renderCuboid(stack, buffer, upwards, front, left, combinedLight, combinedOverlay, -2, 2, 0,
+                        3.008f, 3.008f, 3.008f);
+        }
+    }
+
+    private void renderCuboid(PoseStack stack, MultiBufferSource buffer, Direction upwards, Direction front,
+                              Direction left, int combinedLight, int combinedOverlay, int offsetFront, int offsetUp,
+                              int offsetLeft, float scaleFactorFB, float scaleFactorUD,
+                              float scaleFactorLR) {
+        // Setup
         stack.pushPose();
         var modelManager = Minecraft.getInstance().getModelManager();
         BakedModel cube = modelManager.getModel(CUBE);
-        Vec3i movement = upwards.getNormal().offset(front.getOpposite().getNormal());
-        stack.scale(1.004f, 1.004f, 1.004f);
+
+        var upwardsNormal = upwards.getNormal();
+        var frontNormal = front.getNormal();
+        var leftNormal = left.getNormal();
+
+        // Calculate offset
+        Vec3i movement = upwardsNormal.multiply(offsetUp);
+        // MoniLabs.LOGGER.info("movement after upwards offset: {}", movement);
+        movement = movement.offset(frontNormal.multiply(offsetFront));
+        // MoniLabs.LOGGER.info("movement after front offset: {}", movement);
+        movement = movement.offset(leftNormal.multiply(offsetLeft));
+        // MoniLabs.LOGGER.info("movement after left offset: {}", movement);
+
+        // Calculate scaling factors
+        float scaleFactorX = 0.0f;
+        float scaleFactorY = 0.0f;
+        float scaleFactorZ = 0.0f;
+
+        if (leftNormal.getX() != 0) {
+            scaleFactorX = scaleFactorLR;
+        } else if (leftNormal.getY() != 0) {
+            // noinspection
+            scaleFactorY = scaleFactorLR;
+        } else if (leftNormal.getZ() != 0) {
+            scaleFactorZ = scaleFactorLR;
+        }
+
+        if (frontNormal.getX() != 0) {
+            scaleFactorX = scaleFactorFB;
+        } else if (frontNormal.getY() != 0) {
+            scaleFactorY = scaleFactorFB;
+        } else if (frontNormal.getZ() != 0) {
+            scaleFactorZ = scaleFactorFB;
+        }
+
+        if (upwardsNormal.getX() != 0) {
+            scaleFactorX = scaleFactorUD;
+        } else if (upwardsNormal.getY() != 0) {
+            scaleFactorY = scaleFactorUD;
+        } else if (upwardsNormal.getZ() != 0) {
+            scaleFactorZ = scaleFactorUD;
+        }
+
+        // Actually do the transformations
         stack.translate(movement.getX() + 0.5f, movement.getY() + 0.5f, movement.getZ() + 0.5f);
+        stack.scale(scaleFactorX, scaleFactorY, scaleFactorZ);
         PoseStack.Pose pose = stack.last();
-        VertexConsumer consumer = bufferSource.getBuffer(RenderType.endPortal());
+
+        // Send buffer data, clean up
+        VertexConsumer consumer = buffer.getBuffer(RenderType.endPortal());
         List<BakedQuad> quads = cube.getQuads(null, null, GTValues.RNG, ModelData.EMPTY, null);
         for (BakedQuad quad : quads) {
             consumer.putBulkData(pose, quad, 1.0f, 1.0f, 1.0f, combinedLight, combinedOverlay);
