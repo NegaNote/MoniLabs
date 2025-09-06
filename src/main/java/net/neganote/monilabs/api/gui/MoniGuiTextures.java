f6d4c76fb3fb7c070c8a7f1ab52399bdfd55b619
DragonDuck24 <drake@marinodev.com>
2025-09-06 17:53:02 -0500

    add first texture

---
+++ src/main/java/net/neganote/monilabs/api/gui/MoniGuiTextures.java
@@ -0,0 +1,9 @@
+package net.neganote.monilabs.api.gui;
+
+import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
+
+public class MoniGuiTextures {
+
+    public static final ResourceTexture PROGRESS_BAR_SIMULATION = new ResourceTexture(
+            "monilabs:textures/gui/progress_bar/progress_bar_simulation.png");
+}
