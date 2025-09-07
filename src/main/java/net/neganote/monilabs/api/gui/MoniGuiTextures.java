8b36b5727cd2dea5c1719b5da2648a5d1298e58d
DragonDuck24 <drake@marinodev.com>
2025-09-06 19:40:30 -0500

    add all textures

--- src/main/java/net/neganote/monilabs/api/gui/MoniGuiTextures.java
+++ src/main/java/net/neganote/monilabs/api/gui/MoniGuiTextures.java
@@ -4,6 +4,12 @@ import com.lowdragmc.lowdraglib.gui.texture.ResourceTexture;
 
 public class MoniGuiTextures {
 
+    public static final ResourceTexture PROGRESS_BAR_RECONSTRUCTION = new ResourceTexture(
+            "monilabs:textures/gui/progress_bar/progress_bar_reconstruction.png");
+    public static final ResourceTexture PROGRESS_BAR_ROCKET = new ResourceTexture(
+            "monilabs:textures/gui/progress_bar/progress_bar_rocket.png");
     public static final ResourceTexture PROGRESS_BAR_SIMULATION = new ResourceTexture(
             "monilabs:textures/gui/progress_bar/progress_bar_simulation.png");
+    public static final ResourceTexture PROGRESS_BAR_XP = new ResourceTexture(
+            "monilabs:textures/gui/progress_bar/progress_bar_xp.png");
 }
