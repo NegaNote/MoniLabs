b86904cfc59c04df1e5fb6e5854a762b125cf9a5
NegaNote <neganote43@gmail.com>
2025-10-09 00:09:15 -0400

    add pss mixin that I've had sitting on this computer for two days

---
+++ src/main/java/net/neganote/monilabs/mixin/PowerSubstationMachineMixin.java
@@ -0,0 +1,30 @@
+package net.neganote.monilabs.mixin;
+
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.common.machine.multiblock.electric.PowerSubstationMachine;
+import net.minecraft.server.level.ServerLevel;
+import net.neganote.monilabs.saveddata.CreativeDataAccessSavedData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = PowerSubstationMachine.class, remap = false)
+public class PowerSubstationMachineMixin extends MetaMachine {
+
+    public PowerSubstationMachineMixin(IMachineBlockEntity holder) {
+        super(holder);
+    }
+
+    // Prevents substations from performing any power transfers while TES is running
+    @Inject(method = "transferEnergyTick()V", at = @At(value = "HEAD"), cancellable = true)
+    public void monilabs$injectBeforeTransferEnergyTick(CallbackInfo ci) {
+        if (getLevel() instanceof ServerLevel serverLevel) {
+            CreativeDataAccessSavedData savedData = CreativeDataAccessSavedData.getOrCreate(serverLevel);
+            if (savedData.isEnabledFor(getOwnerUUID())) {
+                ci.cancel();
+            }
+        }
+    }
+}
