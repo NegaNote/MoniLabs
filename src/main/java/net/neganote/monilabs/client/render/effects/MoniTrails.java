b9001a773a8da89d7e4798fb5a55bcc1d3428aaa
NegaNote <neganote43@gmail.com>
2025-10-09 19:10:42 -0400

    spotless

--- src/main/java/net/neganote/monilabs/client/render/effects/MoniTrails.java
+++ src/main/java/net/neganote/monilabs/client/render/effects/MoniTrails.java
@@ -15,10 +15,10 @@ public abstract class MoniTrails {
 
     public static void init() {
         CHROMA_BACKGROUND = CloudChamberUtil
-            .registerBackgroundTrail(MoniLabs.id("chroma"),
-                    new SimpleTrail(20, 0, .1, .1, ParticleTypes.CHROMA_BACKGROUND));
+                .registerBackgroundTrail(MoniLabs.id("chroma"),
+                        new SimpleTrail(20, 0, .1, .1, ParticleTypes.CHROMA_BACKGROUND));
 
         CHROMA_BETA = new SimpleTrail(20, 0, 0, 0,
-            ParticleTypes.CHROMA_BETA);
+                ParticleTypes.CHROMA_BETA);
     }
 }
