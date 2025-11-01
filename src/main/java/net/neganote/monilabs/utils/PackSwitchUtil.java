0f8d0df1fa68be02c5fa9a4b94debf503bcefe22
DragonDuck24 <dragonduckymarino@gmail.com>
2025-10-31 22:58:39 -0500

    I did not fix imports very well :skull:

--- src/main/java/net/neganote/monilabs/utils/PackSwitchUtil.java
+++ src/main/java/net/neganote/monilabs/utils/PackSwitchUtil.java
@@ -3,6 +3,8 @@ package net.neganote.monilabs.utils;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
