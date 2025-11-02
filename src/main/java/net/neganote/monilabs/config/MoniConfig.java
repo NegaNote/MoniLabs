62ffcb176bbfeebe96bf94dd50ba2778626aa192
WithersChat <witherschat@gmail.com>
2025-11-02 23:17:41 +0100

    I think I'm done now?

--- src/main/java/net/neganote/monilabs/config/MoniConfig.java
+++ src/main/java/net/neganote/monilabs/config/MoniConfig.java
@@ -30,13 +30,6 @@ public class MoniConfig {
         public double omnicSynthesizerExponent = 1.557;
 
         @Configurable
-        @Configurable.Comment({ "Minimum multiplier to antimatter generation (inclusive)." })
-        public float antimatterRandomMinInclusive = 0.5f;
-        @Configurable
-        @Configurable.Comment({ "Maximum multiplier to antimatter generation (exclusive)." })
-        public float antimatterRandomMaxExclusive = 2.0f;
-
-        @Configurable
         @Configurable.Comment({ "Whether the microminer is returned upon a microverse reaching zero integrity." })
         public boolean microminerReturnedOnZeroIntegrity = true;
 
