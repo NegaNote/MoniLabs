76bd63bda5d44d5fe70758d69533bfc8cced51a1
Phoenixvine <82596737+Phoenixvine32908@users.noreply.github.com>
2026-01-29 01:03:07 -0500

    Disabled extended facing on the prismatic crucible. (#41)
    * Disable extended facing on the prismatic crucible.

--- src/main/java/net/neganote/monilabs/common/machine/MoniMachines.java
+++ src/main/java/net/neganote/monilabs/common/machine/MoniMachines.java
@@ -284,6 +284,7 @@ public class MoniMachines {
     public static MultiblockMachineDefinition PRISMATIC_CRUCIBLE = REGISTRATE
             .multiblock("prismatic_crucible", PrismaticCrucibleMachine::new)
             .rotationState(RotationState.NON_Y_AXIS)
+            .allowExtendedFacing(false)
             .recipeTypes(MoniRecipeTypes.CHROMATIC_PROCESSING, MoniRecipeTypes.CHROMATIC_TRANSCENDENCE)
             .recipeModifiers(GTRecipeModifiers.OC_NON_PERFECT)
             .appearanceBlock(MoniBlocks.DIMENSIONAL_STABILIZATION_NETHERITE_CASING)
