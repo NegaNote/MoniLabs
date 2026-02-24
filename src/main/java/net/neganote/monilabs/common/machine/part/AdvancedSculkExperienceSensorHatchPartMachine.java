3b7595ea0eadb9c33830a50a9c6cd312cb93e99f
NegaNote <neganote43@gmail.com>
2026-02-23 22:43:13 -0500

    advanced svat xp buffer sensor logic

--- src/main/java/net/neganote/monilabs/common/machine/part/AdvancedSculkExperienceSensorHatchPartMachine.java
+++ src/main/java/net/neganote/monilabs/common/machine/part/AdvancedSculkExperienceSensorHatchPartMachine.java
@@ -2,9 +2,16 @@ package net.neganote.monilabs.common.machine.part;
 
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 
+import net.minecraft.core.Direction;
+import net.neganote.monilabs.common.machine.multiblock.SculkVatMachine;
+
 import com.mojang.blaze3d.MethodsReturnNonnullByDefault;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -12,12 +19,58 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class AdvancedSculkExperienceSensorHatchPartMachine extends SculkExperienceSensorHatchPartMachine {
 
+    public static int DEFAULT_MIN_PERCENT = 33;
+    public static int DEFAULT_MAX_PERCENT = 66;
+
+    @Persisted
+    @Setter
+    @Getter
+    public int minPercent, maxPercent;
+
+    @Persisted
+    @Setter
+    @Getter
+    public boolean inverted;
+
     public static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(
             AdvancedSculkExperienceSensorHatchPartMachine.class,
             SculkExperienceSensorHatchPartMachine.MANAGED_FIELD_HOLDER);
 
     public AdvancedSculkExperienceSensorHatchPartMachine(IMachineBlockEntity holder) {
         super(holder);
+        minPercent = DEFAULT_MIN_PERCENT;
+        maxPercent = DEFAULT_MAX_PERCENT;
+    }
+
+    @Override
+    public int getOutputSignal(@Nullable Direction direction) {
+        if (direction != getFrontFacing().getOpposite()) {
+            return 0;
+        }
+
+        var controller = (SculkVatMachine) getController();
+        if (controller == null) {
+            return 0;
+        }
+
+        int buffer = controller.getXpBuffer();
+
+        int minXp = (int) (SculkVatMachine.XP_BUFFER_MAX * (minPercent / 100.0));
+        int maxXp = (int) (SculkVatMachine.XP_BUFFER_MAX * (maxPercent / 100.0));
+
+        if (inverted) {
+            if (buffer >= minXp && buffer <= maxXp) {
+                return 0;
+            } else {
+                return 15;
+            }
+        } else {
+            if (buffer >= minXp && buffer <= maxXp) {
+                return 15;
+            } else {
+                return 0;
+            }
+        }
     }
 
     @Override
