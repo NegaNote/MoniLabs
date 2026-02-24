6575892da3ce8dda262cffe64cb0538ac9cb4bef
NegaNote <neganote43@gmail.com>
2026-02-23 22:26:01 -0500

    advanced type sensor logic

--- src/main/java/net/neganote/monilabs/common/machine/part/AdvancedMicroverseTypeSensorHatchPartMachine.java
+++ src/main/java/net/neganote/monilabs/common/machine/part/AdvancedMicroverseTypeSensorHatchPartMachine.java
@@ -2,9 +2,18 @@ package net.neganote.monilabs.common.machine.part;
 
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 
+import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
+import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
 
+import net.minecraft.core.Direction;
+import net.neganote.monilabs.common.machine.multiblock.Microverse;
+import net.neganote.monilabs.common.machine.multiblock.MicroverseProjectorMachine;
+
 import com.mojang.blaze3d.MethodsReturnNonnullByDefault;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -12,12 +21,44 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 public class AdvancedMicroverseTypeSensorHatchPartMachine extends MicroverseTypeSensorHatchPartMachine {
 
+    @Setter
+    @Getter
+    @Persisted
+    @DescSynced
+    public Microverse detectorMicroverse;
+
+    @Setter
+    @Getter
+    @Persisted
+    @DescSynced
+    public boolean inverted;
+
     public static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(
             AdvancedMicroverseTypeSensorHatchPartMachine.class,
             MicroverseTypeSensorHatchPartMachine.MANAGED_FIELD_HOLDER);
 
     public AdvancedMicroverseTypeSensorHatchPartMachine(IMachineBlockEntity holder) {
         super(holder);
+    }
+
+    @Override
+    public int getOutputSignal(@Nullable Direction direction) {
+        if (direction == getFrontFacing().getOpposite()) {
+            var controller = (MicroverseProjectorMachine) getController();
+
+            if (controller == null) {
+                return 0;
+            }
+
+            var projectorMicroverse = controller.getMicroverse();
+            if (inverted) {
+                return projectorMicroverse == detectorMicroverse ? 0 : 15;
+            } else {
+                return projectorMicroverse == detectorMicroverse ? 15 : 0;
+            }
+        } else {
+            return 0;
+        }
     }
 
     @Override
