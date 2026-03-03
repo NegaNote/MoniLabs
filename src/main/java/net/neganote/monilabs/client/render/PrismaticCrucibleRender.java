a778a157a40d0213af53dfedcccea4373baa3bdf
Connor Smith <phoenixvine32908@gmail.com>
2026-03-02 20:49:46 -0500

    Goodbye prismaC render rotation.

--- src/main/java/net/neganote/monilabs/client/render/PrismaticCrucibleRender.java
+++ src/main/java/net/neganote/monilabs/client/render/PrismaticCrucibleRender.java
@@ -15,6 +15,8 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.RenderTypeHelper;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.neganote.monilabs.common.machine.multiblock.PrismaticCrucibleMachine;
@@ -50,41 +52,35 @@ public class PrismaticCrucibleRender extends DynamicRender<PrismaticCrucibleMach
     public PrismaticCrucibleRender() {}
 
     @Override
+    @OnlyIn(Dist.CLIENT)
     public void render(PrismaticCrucibleMachine pcm, float partialTicks, PoseStack poseStack, MultiBufferSource buffer,
                        int combinedLight, int combinedOverlay) {
         var level = pcm.getLevel();
         var color = pcm.getColorState();
         assert level != null;
-
-        Direction resolvedUp = RelativeDirection.UP.getRelative(
-                pcm.getFrontFacing(),
-                pcm.getUpwardsFacing(),
-                pcm.isFlipped());
-
-        Direction faceDir = resolvedUp.getAxis() == Direction.Axis.Y ? resolvedUp : resolvedUp.getOpposite();
-
-        Direction down = faceDir.getOpposite();
-
         poseStack.pushPose();
         PoseStack.Pose pose = poseStack.last();
-
         var fluidRenderType = ItemBlockRenderTypes.getRenderLayer(GTMaterials.Iron.getFluid().defaultFluidState());
         var consumer = buffer.getBuffer(RenderTypeHelper.getEntityRenderType(fluidRenderType, false));
         consumer = consumer.color(color.r, color.g, color.b, 1.0F);
+        var up = RelativeDirection.UP.getRelative(pcm.getFrontFacing(), pcm.getUpwardsFacing(),
+                pcm.isFlipped());
+        if (up.getAxis() != Direction.Axis.Y) up = up.getOpposite();
 
-        drawPlane(faceDir, resolvedUp, pcm.getFluidBlockOffsets(), pose.pose(), consumer, GTMaterials.Iron.getFluid(),
+        drawPlane(up, pcm.getFluidBlockOffsets(), pose.pose(), consumer, GTMaterials.Iron.getFluid(),
                 RenderUtil.FluidTextureType.STILL, combinedOverlay, pcm);
-
         poseStack.popPose();
 
         long gameTime = Objects.requireNonNull(Minecraft.getInstance().player).tickCount;
+
+        Direction down = up.getOpposite();
 
         if (pcm.isActive() && pcm.getFocusPos() != null) {
             Vector3f ray = new Vector3f(6.0F * (float) down.getNormal().getX(),
                     6.0F * (float) down.getNormal().getY(),
                     6.0F * (float) down.getNormal().getZ());
 
-            float xOffset = (float) (pcm.getFocusPos().getX() - pcm.getPos().getX()) + 12.0F;
+            float xOffset = (float) (pcm.getFocusPos().getX() - pcm.getPos().getX()) + 0.5F;
             float yOffset = (float) (pcm.getFocusPos().getY() - pcm.getPos().getY()) + 0.5F;
             float zOffset = (float) (pcm.getFocusPos().getZ() - pcm.getPos().getZ()) + 0.5F;
 
@@ -93,64 +89,24 @@ public class PrismaticCrucibleRender extends DynamicRender<PrismaticCrucibleMach
         }
     }
 
-    private BlockPos remapOffset(BlockPos offset, Direction resolvedUp) {
-        if (resolvedUp.getAxis() == Direction.Axis.Y) return offset;
-
-        int x = offset.getX();
-        int y = offset.getY();
-        int z = offset.getZ();
-
-        int sign = resolvedUp.getAxisDirection() == Direction.AxisDirection.POSITIVE ? 1 : -1;
-
-        return switch (resolvedUp.getAxis()) {
-            case X -> new BlockPos(y * sign, 0, z);
-            case Z -> new BlockPos(x, 0, y * sign);
-            default -> offset;
-        };
-    }
-
     // Stolen and modified from FluidBlockRender
-    public void drawPlane(Direction face, Direction resolvedUp, Collection<BlockPos> offsets, Matrix4f pose,
-                          VertexConsumer consumer, Fluid fluid, RenderUtil.FluidTextureType texture,
-                          int combinedOverlay, PrismaticCrucibleMachine pcm) {
+    public void drawPlane(Direction face, Collection<BlockPos> offsets, Matrix4f pose, VertexConsumer consumer,
+                          Fluid fluid, RenderUtil.FluidTextureType texture, int combinedOverlay,
+                          PrismaticCrucibleMachine pcm) {
         var fluidClientInfo = IClientFluidTypeExtensions.of(fluid);
         var sprite = texture.map(fluidClientInfo);
         float u0 = sprite.getU0(), v0 = sprite.getV0(), u1 = sprite.getU1(), v1 = sprite.getV1();
         int color = pcm.getColorState().integerColor;
         int r = red(color), g = green(color), b = blue(color), a = alpha(color);
-
         var normal = getNormal(face);
         var vertices = transformVertices(getVertices(face), face);
 
-        Direction front = pcm.getFrontFacing();
-        Direction upDir = resolvedUp;
-
-        Vector3f leftVec = new Vector3f(
-                (float) upDir.getStepY() * front.getStepZ() - (float) upDir.getStepZ() * front.getStepY(),
-                (float) upDir.getStepZ() * front.getStepX() - (float) upDir.getStepX() * front.getStepZ(),
-                (float) upDir.getStepX() * front.getStepY() - (float) upDir.getStepY() * front.getStepX());
-
-        float leftShift = 0.0f;
-        float backShift = -8.0f;
-
-        Matrix4f shiftedPose = new Matrix4f(pose);
-        shiftedPose.translate(
-                (leftVec.x() * leftShift) + (front.getStepX() * backShift),
-                (leftVec.y() * leftShift) + (front.getStepY() * backShift),
-                (leftVec.z() * leftShift) + (front.getStepZ() * backShift));
-
+        BlockPos prevOffset = null;
         for (var offset : offsets) {
-            float dx = (leftVec.x() * -offset.getX()) + (upDir.getStepX() * offset.getY()) +
-                    (front.getStepX() * offset.getZ());
-            float dy = (leftVec.y() * -offset.getX()) + (upDir.getStepY() * offset.getY()) +
-                    (front.getStepY() * offset.getZ());
-            float dz = (leftVec.z() * -offset.getX()) + (upDir.getStepZ() * offset.getY()) +
-                    (front.getStepZ() * offset.getZ());
-
-            Matrix4f blockPose = new Matrix4f(shiftedPose);
-            blockPose.translate(dx, dy, dz);
-
-            drawFace(blockPose, consumer, vertices, normal, u0, u1, v0, v1, r, g, b, a, combinedOverlay);
+            BlockPos currOffset = prevOffset == null ? offset : offset.subtract(prevOffset);
+            pose.translate(currOffset.getX(), currOffset.getY(), currOffset.getZ());
+            drawFace(pose, consumer, vertices, normal, u0, u1, v0, v1, r, g, b, a, combinedOverlay);
+            prevOffset = offset;
         }
     }
 
