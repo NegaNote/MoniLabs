b8c91a96d439ff15edbb91f48833e80eaca21bca
NegaNote <neganote43@gmail.com>
2026-06-04 22:26:35 -0400

    switch to an overwrite because injections don't work on interfaces

--- src/main/java/net/neganote/monilabs/mixin/RotorHolderMachineMixin.java
+++ src/main/java/net/neganote/monilabs/mixin/RotorHolderMachineMixin.java
@@ -3,15 +3,15 @@ package net.neganote.monilabs.mixin;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IRotorHolderMachine;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = IRotorHolderMachine.class, remap = false)
 public interface RotorHolderMachineMixin {
 
-    @Inject(method = "damageRotor", at = @At(value = "HEAD"), cancellable = true)
-    default void monilabs$stopRotorDamage(int damageAmount, CallbackInfo ci) {
-        ci.cancel();
-    }
+    /**
+     * @author NegaNote
+     * @reason killing rotor damage, can't use an injection on an interface
+     */
+    @Overwrite
+    default void damageRotor(int damageAmount) {}
 }
