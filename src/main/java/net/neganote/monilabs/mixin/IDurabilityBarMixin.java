c58022584dcf72fddd0e33cd4e463bc78117e52e
NegaNote <neganote43@gmail.com>
2026-06-04 23:24:51 -0400

    remove text in large turbine about rotor durability (also spotless)

--- src/main/java/net/neganote/monilabs/mixin/IDurabilityBarMixin.java
+++ src/main/java/net/neganote/monilabs/mixin/IDurabilityBarMixin.java
@@ -3,14 +3,17 @@ package net.neganote.monilabs.mixin;
 import com.gregtechceu.gtceu.api.item.component.IDurabilityBar;
 import com.gregtechceu.gtceu.client.renderer.item.ToolChargeBarRenderer;
 import com.gregtechceu.gtceu.common.item.TurbineRotorBehaviour;
+
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.item.ItemStack;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = IDurabilityBar.class, remap = false)
 public interface IDurabilityBarMixin {
+
     /**
      * @author NegaNote
      * @reason must use overwrite to remove durability bar from turbine rotors because interfaces cannot use injections
